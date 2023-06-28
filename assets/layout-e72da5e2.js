a81953df70de156ca3b240e5a8a3e50ab27fe2d3
JavierBalonga <JavierBalonga@users.noreply.github.com>
2023-06-28 14:11:31 +0000

    deploy: f1c090470b10a02954c5f4295063b08905684e9d

---
+++ assets/layout-e72da5e2.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-5aca1d96.js";function r(){return t.jsx("main",{className:"grow w-full flex justify-center items-center",children:t.jsx(e,{})})}export{r as default};
