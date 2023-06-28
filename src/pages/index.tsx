9e7c7163510f7a9941a547414abe85d1ee728ce5
JavierBalonga <javierbalonga@gmail.com>
2023-06-28 10:31:01 -0300

    fix(*) HashRouter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">HomePage</h1>
       <p className="text-xl">This page is under development.</p>
+      <Link to="/rothiotome">Go to Channel</Link>
     </>
   );
 }
