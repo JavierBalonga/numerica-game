85d2a335131459b2c3829ce5ed1b9ec44bbb8582
JavierBalonga <javierbalonga@gmail.com>
2023-06-29 14:19:18 -0300

    reafctor(GamePage) little refactor

--- src/pages/[channel]/index.tsx
+++ src/pages/[channel]/index.tsx
@@ -30,6 +30,22 @@ const useStore = create(
 export default function GamePage() {
   const { channel } = useParams<{ channel: string }>();
 
+  const [twitchClient, setTwitchClient] = useState<tmi.Client | null>(null);
+
+  useEffect(() => {
+    if (!channel) return;
+
+    const twitchClient = new tmi.Client({ channels: [channel] });
+
+    twitchClient.connect().catch(console.error);
+
+    setTwitchClient(twitchClient);
+
+    return () => {
+      twitchClient.disconnect().catch(console.error);
+    };
+  }, [channel]);
+
   const { maxScore, registerNewScore } = useStore();
   const [state, setState] = useState({
     status: Status.IDLE,
@@ -37,53 +53,54 @@ export default function GamePage() {
     user: "",
   });
 
-  const handleNewMessage = useCallback((user: string, message: string) => {
-    const number = Number(message);
-    const isFiniteNumber = isFinite(number);
-    const isIntegerNumber = number % 1 === 0;
-    const isPositiveNumber = number > 0;
-    if (!isFiniteNumber || !isIntegerNumber || !isPositiveNumber) return;
-    setState((prev) => {
-      if (prev.user === user) return prev;
-      const isSuccess = number === prev.number + 1;
-      if (isSuccess) {
-        registerNewScore(number);
-        return {
-          status: Status.STARTED,
-          number: prev.number + 1,
-          user: user,
-        };
-      } else {
-        return {
-          status: prev.number === 0 ? Status.IDLE : Status.GAME_OVER,
-          number: 0,
-          user: user,
-        };
-      }
-    });
-  }, []);
+  const handleNewMessage = useCallback(
+    (
+      _channel: string,
+      tags: tmi.ChatUserstate,
+      message: string,
+      self: boolean
+    ) => {
+      if (self) return;
+
+      const user = tags.displayName || tags.username;
+      if (!user) return;
+
+      const number = Number(message);
+      const isFiniteNumber = isFinite(number);
+      const isIntegerNumber = number % 1 === 0;
+      const isPositiveNumber = number > 0;
+      if (!isFiniteNumber || !isIntegerNumber || !isPositiveNumber) return;
+
+      setState((prev) => {
+        if (prev.user === user) return prev;
+
+        const isSuccess = number === prev.number + 1;
+        if (isSuccess) {
+          registerNewScore(number);
+          return {
+            status: Status.STARTED,
+            number: prev.number + 1,
+            user: user,
+          };
+        } else {
+          return {
+            status: prev.number === 0 ? Status.IDLE : Status.GAME_OVER,
+            number: 0,
+            user: user,
+          };
+        }
+      });
+    },
+    [setState, registerNewScore]
+  );
 
   useEffect(() => {
-    if (!channel) return;
-
-    const twitchClient = new tmi.Client({
-      options: { debug: true },
-      channels: [channel],
-    });
-
-    twitchClient.connect().catch(console.error);
-
-    twitchClient.on("message", (_channel, tags, message, self) => {
-      const { username, "display-name": displayName } = tags;
-      const user = displayName || username;
-      if (self || !user) return;
-      handleNewMessage(user, message);
-    });
-
+    if (!twitchClient) return;
+    twitchClient.on("message", handleNewMessage);
     return () => {
-      twitchClient.disconnect().catch(console.error);
+      twitchClient.removeListener("message", handleNewMessage);
     };
-  }, [channel]);
+  }, [twitchClient, handleNewMessage]);
 
   return (
     <div className="flex flex-col items-center justify-between gap-4 border-8 border-rose-400 p-8 rounded-xl h-full w-full grow max-h-[300px] max-w-[300px] bg-slate-900">
